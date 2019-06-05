8dd7d9436c4aa58b7a623d73f732a11f86e51b04
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-05 15:13:04 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Custom_Colors_and_Sounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
